ba8e77df8dddd5b36edab91c74d81e251a4f30fa
AdnanIqbalKhan <hafizadnaniqbalkhan@gmail.com>
2025-08-18 21:49:23 +0500

    fix deps

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
 import { features } from "@/lib/env";
 import { NextResponse } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 let middleware;
 if (features.clerk) {
-  const clerkMiddleware = await import("@clerk/nextjs/server").then(
-    (mod) => mod.clerkMiddleware
-  );
   middleware = clerkMiddleware();
 } else {
   middleware = function () {
